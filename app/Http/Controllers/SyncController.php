d5a14160e7cbf4597f99f3132e923ffdf2cab898
Edwin Carmona <eedwincarmona@gmail.com>
2020-08-06 09:27:37 -0500

    Update SyncController.php

--- app/Http/Controllers/SyncController.php
+++ app/Http/Controllers/SyncController.php
@@ -20,14 +20,14 @@ class SyncController extends Controller
 
     public function syncronizeWithERP($lastSyncDate = "")
     {
-        $jsonString = file_get_contents(base_path('response_from_siie.json'));
-        // $client = new Client([
-        //     'base_uri' => 'localhost:9000',
-        //     'timeout' => 10.0,
-        // ]);
+        // $jsonString = file_get_contents(base_path('response_from_siie.json'));
+        $client = new Client([
+            'base_uri' => 'localhost:9000',
+            'timeout' => 10.0,
+        ]);
 
-        // $response = $client->request('GET', 'getInfoERP/2019-01-01 00:00:00');
-        // $jsonString = $response->getBody()->getContents();
+        $response = $client->request('GET', 'getInfoERP/2019-01-01 00:00:00');
+        $jsonString = $response->getBody()->getContents();
         $data = json_decode($jsonString);
 
         // dd($data);
@@ -46,8 +46,8 @@ class SyncController extends Controller
         $absCont = new incidentController();
         $absCont->saveAbsencesFromJSON($data->absences);
 
-        // $prepayCont = new prePayrollController();
-        // $prepayCont->saveCutCalendarFromJSON($data->cuts);
+        $prepayCont = new prePayrollController();
+        $prepayCont->saveCutCalendarFromJSON($data->cuts);
 
         $newDate = Carbon::now();
         $newDate->subMinutes(30);
