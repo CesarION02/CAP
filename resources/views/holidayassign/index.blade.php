5b3b8496639e0d585d588d65df6a1ac375502049
Edwin Carmona <eedwincarmona@gmail.com>
2020-09-03 09:08:55 -0500

    Filtro de rango de fechas en festivos y asignación de festivos

--- resources/views/holidayassign/index.blade.php
+++ resources/views/holidayassign/index.blade.php
@@ -3,18 +3,23 @@
 Asignar días festivos
 @endsection
 
+@section('styles1')
+    <link rel="stylesheet" href="{{asset("daterangepicker/daterangepicker.css")}}">
+@endsection
+
 @section("scripts")
-<script src="{{asset("assets/pages/scripts/admin/datatable/index.js")}}" type="text/javascript"></script>
-<script src="{{ asset("dt/datatables.js") }}" type="text/javascript"></script>
-<script src="{{ asset('dt/dataTables.buttons.min.js') }}"></script>
-<script src="{{ asset("assets/js/moment/moment.js") }}" type="text/javascript"></script>
-<script src="{{ asset("assets/js/moment/datetime-moment.js") }}" type="text/javascript"></script>
+    <script src="{{asset("assets/pages/scripts/admin/datatable/index.js")}}" type="text/javascript"></script>
+    <script src="{{ asset("dt/datatables.js") }}" type="text/javascript"></script>
+    <script src="{{ asset('dt/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset("assets/js/moment/moment.js") }}" type="text/javascript"></script>
+    <script src="{{ asset("assets/js/moment/datetime-moment.js") }}" type="text/javascript"></script>
 	<script src="{{ asset('dt/buttons.flash.min.js') }}"></script>
 	<script src="{{ asset('dt/jszip.min.js') }}"></script>
 	<script src="{{ asset('dt/pdfmake.min.js') }}"></script>
 	<script src="{{ asset('dt/vfs_fonts.js') }}"></script>
 	<script src="{{ asset('dt/buttons.html5.min.js') }}"></script>
-	<script src="{{ asset('dt/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('dt/buttons.print.min.js') }}"></script>
+    <script src="{{ asset("daterangepicker/daterangepicker.js") }}" type="text/javascript"></script>
 <script>
     $(document).ready( function () {
         $.fn.dataTable.moment('DD/MM/YYYY');
@@ -61,6 +66,37 @@ Asignar días festivos
     });
 </script>
 
+<script>
+    $(function() {
+        var start = moment(<?php echo json_encode($start_date) ?>);
+        var end = moment(<?php echo json_encode($end_date) ?>);
+
+        function cb(start, end) {
+            $('#reportrange span').html(start.format('D MMMM YYYY') + ' - ' + end.format('D MMMM YYYY'));
+            document.getElementById("start-date").value = start.format('YYYY-MM-DD');
+            document.getElementById("end-date").value = end.format('YYYY-MM-DD');
+        }
+
+        $('#reportrange').daterangepicker({
+            startDate: start,
+            endDate: end,
+            ranges: {
+                'Este mes': [moment().startOf('month'), moment().endOf('month')],
+                'Mes pasado': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+                'Este año': [moment().startOf('year'), moment().endOf('year')],
+                'Año pasado': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')]
+            }
+        }, cb);
+
+        cb(start, end);
+    });
+
+    $('#reportrange').on('apply.daterangepicker', function(ev, picker) {
+        document.getElementById("start-date").value = picker.startDate.format('YYYY-MM-DD');
+        document.getElementById("end-date").value = picker.endDate.format('YYYY-MM-DD');
+    });
+</script>
+
 @endsection
 
 @section('content')
@@ -71,21 +107,45 @@ Asignar días festivos
             <div class="box-header with-border">
                 <h3 class="box-title">Asignar días festivos</h3>
                 @include('layouts.usermanual', ['link' => "http://192.168.1.233:8080/dokuwiki/doku.php?id=wiki:asignacionfestivos"])
-                <div class="box-tools pull-right">
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-info">Asignar</button>
-                      
-                        <button type="button" class="btn btn-info dropdown-toggle"
-                                data-toggle="dropdown">
-                          <span class="caret"></span>
-                          <span class="sr-only">Desplegar menú</span>
-                        </button>
-                      
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="{{route('crear_asignacion_festivo','1')}}">Por empleado</a>
-                            <li><a href="{{route('crear_asignacion_festivo','2')}}">Por departamento CAP</a>
-                                <li><a href="{{route('crear_asignacion_festivo','3')}}">Por área</a>
-                        </ul>
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-4">
+                        <div class="row">
+                            <div class="col-md-9">
+                                <form action="{{ route('asignacion_festivo') }}">
+                                    <input type="hidden" id="start-date" name="start_date">
+                                    <input type="hidden" id="end-date" name="end_date">
+                                    <div class="input-group">
+                                        <div id="reportrange" 
+                                            style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%">
+                                            <i class="fa fa-calendar"></i>&nbsp;
+                                            <span></span> <i class="fa fa-caret-down"></i>
+                                        </div>
+                                        <span class="input-group-btn">
+                                            <button class="btn btn-default" type="submit">
+                                                <i class="glyphicon glyphicon-search"></i>
+                                            </button>
+                                        </span>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-info">Asignar</button>
+                                
+                                    <button type="button" class="btn btn-info dropdown-toggle"
+                                            data-toggle="dropdown">
+                                    <span class="caret"></span>
+                                    <span class="sr-only">Desplegar menú</span>
+                                    </button>
+                                
+                                    <ul class="dropdown-menu" role="menu">
+                                    <li><a href="{{route('crear_asignacion_festivo','1')}}">Por empleado</a>
+                                        <li><a href="{{route('crear_asignacion_festivo','2')}}">Por departamento CAP</a>
+                                            <li><a href="{{route('crear_asignacion_festivo','3')}}">Por área</a>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
