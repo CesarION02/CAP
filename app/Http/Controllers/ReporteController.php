55891ac466326ee090bb2328f98b8183c4b3ea2d
Cesar Iván Orozco Natividad <cesar.i.orozco.n@gmail.com>
2021-08-11 12:35:28 -0500

    bug fix alias en registers

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -396,7 +396,7 @@ class ReporteController extends Controller
 
         $register = $register->select('e.num_employee', 'e.name', 'r.date', 'r.time', 'r.type_id', 'e.external_id')
                                 ->whereBetween('r.date', [$startDate, $endDate])
-                                ->where('registers.is_delete',0)
+                                ->where('r.is_delete',0)
                                 ->get();
 
         return view('report.reportRegsView')
