a53cfa205e5f1c0ff9d17bf6727216128c327af7
Edwin Carmona <eedwincarmona@gmail.com>
2021-08-13 11:08:35 -0500

    Obtención de fotos de siie

--- routes/web.php
+++ routes/web.php
@@ -348,6 +348,8 @@ Route::put('biostar/updatebiostarid', 'biostarController@updateBiostarId')->name
 Route::get('permiso-rol', 'permisorolController@index')->name('permiso_rol');
 Route::get('permiso-rol', 'permisorolController@store')->name('guardar_permiso_rol');
 
+/* Fotos de empleados */
+Route::get('empphotos/{id}', 'PhotosController@index')->name('photos.employees');
 
 
 Route::get('/home', 'HomeController@index')->name('home');
