086021080bce77c7c8f7589d23b638826f820bc7
Cesar Iván Orozco Natividad <cesar.i.orozco.n@gmail.com>
2021-08-18 15:56:23 -0500

    cambio nombre mis empleado por fotos mis empleados

--- resources/views/photosemployee/index.blade.php
+++ resources/views/photosemployee/index.blade.php
@@ -9,7 +9,7 @@
             @include('includes.mensaje')
             <div class="box">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Mis Empleados</h3>
+                    <h3 class="box-title">Fotos mis Empleados</h3>
                     @include('layouts.usermanual', ['link' => "http://192.168.1.233:8080/dokuwiki/doku.php"])
                 </div>
                 <div class="box-body">
