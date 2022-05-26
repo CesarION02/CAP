9ec2e3c5a088fc190022acca17ac7b05f7f217a8
Edwin Carmona <eedwincarmona@gmail.com>
2022-05-26 09:32:37 -0500

    Horas extra en horarios determinados

--- app/SData/SDataProcess.php
+++ app/SData/SDataProcess.php
@@ -1344,7 +1344,7 @@ class SDataProcess {
         if (abs($comparisonIn->diffMinutes) <= $config->maxGapSchedule && abs($comparisonOut->diffMinutes) <= $config->maxGapSchedule) {
             $oRow->inDateTimeSch = $inDate.' 18:30:00';
             $oRow->outDateTimeSch = $outDate.' 06:30:00';
-            $oRow->overDefaultMins = 300;
+            $oRow->overDefaultMins = 60;
             return $oRow;
         }
 
@@ -1366,7 +1366,7 @@ class SDataProcess {
         if (abs($comparisonIn->diffMinutes) <= $config->maxGapSchedule && abs($comparisonOut->diffMinutes) <= $config->maxGapSchedule) {
             $oRow->inDateTimeSch = $inDate.' 06:30:00';
             $oRow->outDateTimeSch = $outDate.' 18:30:00';
-            $oRow->overDefaultMins = 240;
+            $oRow->overDefaultMins = 0;
             return $oRow;
         }
 
