496101fe6ebf232cf2841d9f29f2848f26467b16
Edwin Carmona <eedwincarmona@gmail.com>
2022-09-15 13:39:29 -0500

    Update SAuxSchedule.php

--- app/SUtils/SAuxSchedule.php
+++ app/SUtils/SAuxSchedule.php
@@ -4,15 +4,11 @@
         public $inDateTimeSch;
         public $outDateTimeSch;
         public $isSpecialSchedule;
-        public $inDateTimeSch;
-        public $outDateTimeSch;
 
         public function __construct() {
             $this->inDateTimeSch = null;
             $this->outDateTimeSch = null;
             $this->isSpecialSchedule = false;
-            $this->inDateTimeSch = false;
-            $this->outDateTimeSch = false;
         }
     }
 
