8aefcbb8fd1f762131f3977a8bf6fc9b4656fb17
Edwin Carmona <eedwincarmona@gmail.com>
2023-02-20 10:07:07 -0600

    Cambio en los comentarios por tipo de ajuste
    - Se habilita el comentario "cambio de turno"

--- database/migrations/2023_01_17_085739_create_table_adj_vs_comments.php
+++ database/migrations/2023_01_17_085739_create_table_adj_vs_comments.php
@@ -14,7 +14,7 @@ class CreateTableAdjVsComments extends Migration
     public function up()
     {
         DB::table('comments')
-            ->whereIn('id', [4, 15, 8])
+            ->whereIn('id', [4, 8])
             ->update([
                 'is_delete' => '1',
             ]);                            
@@ -50,17 +50,18 @@ class CreateTableAdjVsComments extends Migration
 
             // JUSTIFICAR FALTA
         	['sort_number' => '1', 'comment_id' => '10', 'adjust_type_id' => '4'],
-        	['sort_number' => '1', 'comment_id' => '11', 'adjust_type_id' => '4'],
+        	['sort_number' => '2', 'comment_id' => '11', 'adjust_type_id' => '4'],
 
             // DESCONTAR TIEMPO EXTRA
         	// ['sort_number' => '1', 'comment_id' => '', 'adjust_type_id' => '5'],
 
             // AGREGAR TIEMPO EXTRA
         	['sort_number' => '1', 'comment_id' => '2', 'adjust_type_id' => '6'],
-        	['sort_number' => '1', 'comment_id' => '7', 'adjust_type_id' => '6'],
-        	['sort_number' => '1', 'comment_id' => '13', 'adjust_type_id' => '6'],
-        	['sort_number' => '1', 'comment_id' => '14', 'adjust_type_id' => '6'],
-        	['sort_number' => '1', 'comment_id' => '17', 'adjust_type_id' => '6'],
+        	['sort_number' => '2', 'comment_id' => '7', 'adjust_type_id' => '6'],
+        	['sort_number' => '3', 'comment_id' => '13', 'adjust_type_id' => '6'],
+        	['sort_number' => '4', 'comment_id' => '14', 'adjust_type_id' => '6'],
+        	['sort_number' => '5', 'comment_id' => '17', 'adjust_type_id' => '6'],
+        	['sort_number' => '6', 'comment_id' => '15', 'adjust_type_id' => '6'],
 
             // COMENTARIOS
         	['sort_number' => '1', 'comment_id' => '2', 'adjust_type_id' => '7'],
@@ -77,6 +78,7 @@ class CreateTableAdjVsComments extends Migration
         	['sort_number' => '12', 'comment_id' => '16', 'adjust_type_id' => '7'],
         	['sort_number' => '13', 'comment_id' => '17', 'adjust_type_id' => '7'],
         	['sort_number' => '14', 'comment_id' => '18', 'adjust_type_id' => '7'],
+        	['sort_number' => '15', 'comment_id' => '15', 'adjust_type_id' => '7'],
         ]);
     }
 
@@ -90,7 +92,7 @@ class CreateTableAdjVsComments extends Migration
         Schema::dropIfExists('prepayroll_adjusts_comments');
 
         DB::table('comments')
-            ->whereIn('id', [4, 15, 8])
+            ->whereIn('id', [4, 8])
             ->update([
                 'is_delete' => '0',
             ]); 
