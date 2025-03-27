375c78386a8d03f60e991c8b0c4277ffa90309ef
Edwin Carmona <eedwincarmona@gmail.com>
2025-03-27 09:24:15 -0600

    feat: footer reportes

---
+++ resources/views/mails/footer.blade.php
@@ -0,0 +1,13 @@
+<hr style="border: 1px solid #ccc; margin: 20px 0;">
+<p style="font-size: 12px; color: #555; text-align: center; margin: 0;">
+    Favor de no responder este mail, fue generado de forma automática.
+</p>
+<p style="font-size: 12px; color: #555; text-align: center; margin: 5px 0;">
+    CAP 1.0 ©2016-2025 Software Aplicado SA de CV
+</p>
+<p style="font-size: 12px; color: #555; text-align: center; margin: 5px 0;">
+    <a href="https://www.swaplicado.com.mx" style="color: #555; text-decoration: none;">www.swaplicado.com.mx</a>
+</p>
+<p style="font-size: 12px; color: #555; text-align: center; margin: 5px 0;">
+    CAP 1.0 001.3
+</p>
