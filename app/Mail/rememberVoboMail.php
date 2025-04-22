681e488da906ecb6de6bece127570f6481b2042a
Adrián Avilés <92331312+AdrianAviles@users.noreply.github.com>
2025-04-22 12:52:40 -0600

    fix mail, recordatorio corte de prenomina

--- app/Mail/rememberVoboMail.php
+++ app/Mail/rememberVoboMail.php
@@ -41,7 +41,7 @@ class rememberVoboMail extends Mailable
             $subject = 'Aviso: Falta Vobo de prenómina ' . $this->wayPay . ' #' . $this->num;
         }
 
-        return $this->from('adrian.aviles@swaplicado.com.mx')
+        return $this->from('cap@aeth.mx')
                     ->subject('[CAP] ' . $subject)
                     ->view('mails.rememberVobo')
                     ->with([
