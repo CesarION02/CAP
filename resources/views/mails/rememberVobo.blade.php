bd29a624d959e1ecaca478aca34852d948dd8b78
Adrián Avilés <92331312+AdrianAviles@users.noreply.github.com>
2025-05-05 10:06:54 -0600

    cambio de texto en recordatorio de prenomina

--- resources/views/mails/rememberVobo.blade.php
+++ resources/views/mails/rememberVobo.blade.php
@@ -7,14 +7,19 @@
 </head>
 <body>
     @if ($type == 'preClose')
-        <h2>La fecha de corte de la prenómina {{ $wayPay }} #{{ $num }} es el: {{$endDate}}</h2>
-        <p>{{ $days > 1 ? 'Quedan ' : 'Queda ' }} {{$days}} {{ $days > 1 ? ' días' : ' día' }} para el corte.</p>
+        @if ($days > 0 )
+            <h2>La fecha de corte de la prenómina {{ $wayPay }} #{{ $num }} es el {{$endDate}}.</h2>
+            <p>{{ $days > 1 ? 'Quedan ' : 'Queda ' }} {{$days}} {{ $days > 1 ? ' días' : ' día' }} para el corte.</p>
+        @else
+            <h2>La fecha de corte de la prenómina {{ $wayPay }} #{{ $num }} es hoy, {{$endDate}}.</h2>
+            <p>Favor de revisar y aprobar la prenómina.</p>
+        @endif
     @elseif ($type == 'afterClose')
         @if ($days > 1)
-            <h2>Te recordamos dar tu Vobo de la prenómina {{ $wayPay }} {{ $num }} que cortó el {{$endDate}}</h2>
-            <p>Han transcurrido {{$days}} {{ $days > 1 ? ' días' : ' día' }} del corte</p>
+            <h2>Te recordamos dar tu Vobo de la prenómina {{ $wayPay }} {{ $num }} que cortó el {{$endDate}}.</h2>
+            <p>Han transcurrido {{$days}} {{ $days > 1 ? ' días' : ' día' }} del corte.</p>
         @elseif ($days == 1)
-            <h2>Te recordamos dar tu Vobo de la prenómina {{ $wayPay }} {{ $num }} que cortó ayer</h2>
+            <h2>Te recordamos dar tu Vobo de la prenómina {{ $wayPay }} {{ $num }} que cortó ayer.</h2>
         @endif
     @endif
 
